33c4d512c0dc28e81d89cb841c10a2341ec00147
Milan Malfait <m.malfait@ucl.ac.uk>
2024-08-06 11:41:17 +0100

    Add documentation

--- R/split-string.R
+++ R/split-string.R
@@ -1,3 +1,14 @@
+#' Split a string into a vector of strings
+#'
+#' @param x a character string to be split
+#' @param sep the separator on which to split
+#'
+#' @return a vector of strings
+#' @export
+#'
+#' @examples
+#' splitstring("alfa,bravo,charlie")
+#' splitstring("alfa,bravo charlie", sep = " ")
 splitstring <- function(x, sep = ",") {
     strsplit(x, split = sep)[[1]]
 }
